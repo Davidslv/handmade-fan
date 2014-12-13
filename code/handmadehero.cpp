26788dc41b32490ea37516f4f58f3e1163c8bc18
David Silva <davidslv@gmail.com>
2014-12-13 06:27:51 -0500

    Renames file and formats code

---
+++ code/handmadehero.cpp
@@ -0,0 +1,17 @@
+/*
+ * Handmade Hero by Casey
+ * http://handmadehero.org
+ *
+ * Linux / OSX 
+ * http://davidgow.net/handmadepenguin/ch1.html
+ * */
+
+#include <SDL.h>
+
+int main(int argc, char *argv[]) {
+  SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION,
+                           "Handmade Hero",
+                           "This is Handmade Hero",
+                           0);
+  return 0;
+}
